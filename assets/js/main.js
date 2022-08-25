3b2f09934aca6f0af429d5eb1126fb1367f9fc63
johnazt <john.azt.dev@gmail.com>
2022-08-25 16:53:34 -0400

    Create Button

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,22 +1,40 @@
 const grid = document.querySelector(".grid");
+const btnPopUp = document.getElementById("btn-popup");
+const form = document.getElementById("form");
+const btnEnter = document.getElementById("btn-enter");
 
-const squaresContainer = [];
+let squaresContainer = [];
 
-function createSquares() {
-	for (let i = 0; i < 256; i++) {
-		const square = document.createElement("div");
+function showPopUp () {
+    form.classList.remove("hide");
+    btnEnter.addEventListener("click", (e) => {
+        let inputValue = e.target.previousElementSibling.value;
+        form.classList.add("hide");
+        createSquares(inputValue);
+        colorSquares();
+	});
+}
+
+function createSquares (number) {
+	const conversion = number * number;
+
+	for (let i = 0; i < conversion; i++) {
+        const square = document.createElement("div");
+        grid.style.width = `${number * 10}px`
+        grid.style.height = `${number * 10}px`
 		square.classList.add("grid-square");
 		grid.appendChild(square);
-        squaresContainer.push(square)
+		squaresContainer.push(square);
     }
 }
-createSquares();
 
-function colorSquares () {
-    squaresContainer.forEach(square => {
-        square.addEventListener("mouseover", (e) => {
-            square.classList.add("black")
-        })
-    })
+function colorSquares() {
+	squaresContainer.forEach((square) => {
+		square.addEventListener("mouseover", (e) => {
+            square.classList.add("black");
+		});
+	});
 }
-colorSquares()
+
+
+btnPopUp.addEventListener("click", showPopUp);
