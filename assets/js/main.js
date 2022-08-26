4e1bf0fe78295cfb3dadbca514a2f6055daf4c00
johnazt <john.azt.dev@gmail.com>
2022-08-26 16:33:57 -0400

    Fix bug in Button Erase

--- assets/js/main.js
+++ assets/js/main.js
@@ -2,39 +2,56 @@ const grid = document.querySelector(".grid");
 const btnPopUp = document.getElementById("btn-popup");
 const form = document.getElementById("form");
 const btnEnter = document.getElementById("btn-enter");
+const btnErase = document.getElementById("btn-erase");
 
-let squaresContainer = [];
+const color = "black";
+let size = 20;
+createSquares(size);
 
-function showPopUp () {
-    form.classList.remove("hide");
-    btnEnter.addEventListener("click", (e) => {
-        let inputValue = e.target.previousElementSibling.value;
-        form.classList.add("hide");
-        createSquares(inputValue);
-        colorSquares();
-	});
-}
+// TAKES THE USER INPUT AND CALL CREATESQUARE FUNCTION
+btnEnter.addEventListener("click", (e) => {
+	// SIZE TAKES A NEW VALUE
+	size = e.target.previousElementSibling.value; //
+	grid.innerHTML = "";
+	createSquares(size);
+});
 
-function createSquares (number) {
-	const conversion = number * number;
-
-	for (let i = 0; i < conversion; i++) {
-        const square = document.createElement("div");
-        grid.style.width = `${number * 10}px`
-        grid.style.height = `${number * 10}px`
+// CREATE AND ADD SIZES TO GRID AND SQUARES
+function createSquares(inputValue) {
+	for (let i = 0; i < inputValue ** 2; i++) {
+		const square = document.createElement("div");
 		square.classList.add("grid-square");
+		grid.style.gridTemplateColumns = `repeat(${inputValue}, 2fr)`;
+		grid.style.gridTemplateRows = `repeat(${inputValue}, 2fr)`;
 		grid.appendChild(square);
-		squaresContainer.push(square);
-    }
+	}
 }
 
-function colorSquares() {
-	squaresContainer.forEach((square) => {
-		square.addEventListener("mouseover", (e) => {
-            square.classList.add("black");
+// PAINTING SQUARES
+function paintGrid(elem, color) {
+	if (elem.buttons == 1) {
+		if (elem.target.classList == "grid-square") {
+			let square = elem.target;
+			square.style.backgroundColor = color;
+		}
+	} else {
+		//Exit condition if mouse is not clicked.
+		return;
+	}
+}
+// CONDITION FOR USE MOUSEDOWN WITH MOUSEOVER
+grid.addEventListener("mousedown", (e) => {
+	if (e.buttons == 1) {
+		window.addEventListener("mouseover", (e) => {
+			paintGrid(e, color);
 		});
-	});
+	}
+});
+
+// RESET THE GRID
+function eraseSquare() {
+	grid.innerHTML = "";
+	createSquares(size);
 }
 
-
-btnPopUp.addEventListener("click", showPopUp);
+btnErase.addEventListener("click", eraseSquare);
